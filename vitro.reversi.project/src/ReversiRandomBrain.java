7144a1fb94c9d8f76117260cb1a525ccee452c49
earthtowalt <jackwalton00@aol.com>
2021-09-23 12:04:20 -0500

    actually adding random brain

--- vitro.reversi.project/src/ReversiRandomBrain.java
+++ vitro.reversi.project/src/ReversiRandomBrain.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ReversiRandomBrain {
+// ModelAI Assignment - Reversi
+// Topic: Adversarial  Search
+// File: ReversiGreedyBrain.java
+// Authors: ***
+
+import vitro.*;
+import vitro.grid.*;
+import vitro.util.*;
+import java.util.*;
+import static vitro.util.Groups.*;
+
+public class ReversiRandomBrain implements Agent<Reversi.Player> {
+
+    public Action choose(Reversi.Player actor, Set<Action> options) {
+
+        // pass if needed
+        if (options.size() == 1) {
+            return first(options);
+        }
+        
+        // otherwise select a move that places a piece
+        return options.stream()
+                .filter(a -> a instanceof Reversi.Move)
+                .findAny()
+                .orElse(null);
+
+    }
 }
